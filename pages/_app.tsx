0fdf626dd3d608bd4a2efae6ffac6113641f8bb2
yinonhever <yinonehever@gmail.com>
2023-07-17 21:01:48 +0300

    update color of progress bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import type { AppProps } from "next/app";
 import CartProvider from "@/lib/CartContext";
 import Layout from "@/lib/Layout";
 import { AnimatePresence } from "framer-motion";
-import NextProgressBar from "nextjs-progressbar";
+import ProgressBar from "nextjs-progressbar";
 import NProgress from "nprogress";
 
 NProgress.configure({ showSpinner: false });
@@ -11,7 +11,7 @@ NProgress.configure({ showSpinner: false });
 export default function App({ Component, pageProps, router }: AppProps) {
   return (
     <CartProvider>
-      <NextProgressBar color="#E07C4C" height={4} />
+      <ProgressBar color="#d87d4a" height={4} />
       <Layout>
         <AnimatePresence
           mode="wait"
