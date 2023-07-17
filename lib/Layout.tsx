7f54bdc75ddbca2c546136914d957ba045bf1d0a
yinonhever <yinonehever@gmail.com>
2023-07-17 20:42:18 +0300

    fix issue with page animation caused by the framer motion library

--- lib/Layout.tsx
+++ lib/Layout.tsx
@@ -1,12 +1,16 @@
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, useEffect } from "react";
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
 import { Manrope } from "next/font/google";
-import { cx } from "./functions";
+import { cx, fixTimeoutTransition } from "./functions";
 
 const manrope = Manrope({ subsets: ["latin"] });
 
 export default function Layout({ children }: PropsWithChildren) {
+  useEffect(() => {
+    fixTimeoutTransition(500);
+  }, []);
+
   return (
     <>
       <div className={cx("layout", manrope.className)}>
--- lib/functions.ts
+++ lib/functions.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { Document } from "mongoose";
 import type { DataItem } from "./types";
+import Router from "next/router";
 
 export const fecther = async <T>(url: string) => {
   const { data } = await axios.get<T>(url);
@@ -57,4 +58,40 @@ export const getConvertedItem = <T>(item: Document | DataItem<T>) => {
     convertedItem.updatedAt = new Date(updatedAt).toISOString();
   }
   return convertedItem;
+};
+
+export const fixTimeoutTransition = (timeout: number): void => {
+  Router.events.on("beforeHistoryChange", () => {
+    // Create a clone of every <style> and <link> that currently affects the page. It doesn't matter
+    // if Next.js is going to remove them or not since we are going to remove the copies ourselves
+    // later on when the transition finishes.
+    const nodes = document.querySelectorAll(
+      "link[rel=stylesheet], style:not([media=x])"
+    );
+    const copies = [...nodes].map(el => el.cloneNode(true) as HTMLElement);
+
+    for (const copy of copies) {
+      // Remove Next.js' data attributes so the copies are not removed from the DOM in the route
+      // change process.
+      copy.removeAttribute("data-n-p");
+      copy.removeAttribute("data-n-href");
+
+      // Add duplicated nodes to the DOM.
+      document.head.appendChild(copy);
+    }
+
+    const handler = () => {
+      // Emulate a `.once` method using `.on` and `.off`
+      Router.events.off("routeChangeComplete", handler);
+
+      window.setTimeout(() => {
+        for (const copy of copies) {
+          // Remove previous page's styles after the transition has finalized.
+          document.head.removeChild(copy);
+        }
+      }, timeout);
+    };
+
+    Router.events.on("routeChangeComplete", handler);
+  });
 };
