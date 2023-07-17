7fe8533e56d1a68ff8eef9174a789a46331f8ee4
yinonhever <yinonehever@gmail.com>
2023-07-17 20:59:20 +0300

    remove default spinner on page load

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+
+  devIndicators: {
+    buildActivity: false
+  }
 };
 
 module.exports = nextConfig;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,15 @@ import type { AppProps } from "next/app";
 import CartProvider from "@/lib/CartContext";
 import Layout from "@/lib/Layout";
 import { AnimatePresence } from "framer-motion";
-import NextProgress from "nextjs-progressbar";
+import NextProgressBar from "nextjs-progressbar";
+import NProgress from "nprogress";
+
+NProgress.configure({ showSpinner: false });
 
 export default function App({ Component, pageProps, router }: AppProps) {
   return (
     <CartProvider>
-      <NextProgress color="#E07C4C" height={4} />
+      <NextProgressBar color="#E07C4C" height={4} />
       <Layout>
         <AnimatePresence
           mode="wait"
